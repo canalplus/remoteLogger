72873d7f86f83a5d188bfce95877e8a1cdede065
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2015-05-09 19:15:07 +0200

    First release

---
+++ index.js
@@ -0,0 +1,103 @@
+  var request = require('request');
+
+  var global = {
+    LOGGER_NAME: 'remote',
+    LOGGER_OPTIONS: {
+      url: "http://192.168.1.92:8080"
+    }
+  };
+
+  var LEVELS = ['debug', 'info', 'warning', 'error', 'fatal'];
+
+  var _slice = Array.prototype.slice;
+
+  var COLORS = {
+    debug:   'grey',
+    info:    'dodgerblue',
+    warning: 'orange',
+    error:   'red',
+    fatal:   'maroon'
+  };
+
+  function filter(args, namespace) {
+    var f = global.LOGGER_FILTERS;
+    if (f && f[namespace]) { return; }
+    return _slice.call(args);
+  }
+
+function REMOTE_LOGGER(options) {
+    var flush    = [],
+        size     = options.batchSize || 50,
+        interval = options.batchInterval || 5000,
+        url      = options.url;
+
+    function send() {
+        var logs = flush.splice(0, size);
+//        data = JSON.stringify(logs),
+        function fail() {
+            flush.unshift.apply(flush, logs);
+            setTimeout(send, interval);
+        }
+        var options = {
+          uri: url,
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          json: true,
+          body: logs
+        };
+
+        request(options, function(err, msg, body){
+            if(err) {
+              fail();
+            }
+            else {
+              if(msg.status >= 300) { return fail(); }
+              if (flush.length) { setTimeout(send, interval); }
+            }
+        });
+    }
+    
+    return function(args, namespace, level) {
+        if (!url) { return; }
+        var log = {
+            namespace: namespace,
+            level: level,
+            date: new Date(),
+            args: args
+        };
+        if (flush.push(log) === 1) {
+            setTimeout(send, interval);
+        }
+    };
+}
+
+  var list = {
+//    'console': CONSOLE_LOGGER,
+//    'html':    HTML_LOGGER,
+    'remote':  REMOTE_LOGGER
+  };
+
+  var names = (global.LOGGER_NAME || '').split(',');
+  var loggers = names.map(function(name) {
+    var logger = list[name];
+    return logger && logger(global.LOGGER_OPTIONS || {});
+  });
+
+  function LOGGER(namespace) {
+    var logger = function() {
+      if (!loggers.length) { return; }
+      var args = filter(arguments, namespace);
+      var levl = (this && this.LOG_LEVEL) || 'info';
+      args && loggers.forEach(function(l) {
+        l(args, namespace, levl);
+      });
+    };
+    return logger;
+  }
+
+var LOG = LOGGER('test');
+
+LOG('this is a test');
+
